6cc0a162f1fe0668b08763d81a89c3fcae4ccb7f
Keynan <keynan21@gmail.com>
2018-08-26 00:57:37 +1000

    operating on tagged_t; complient with bs-platform 2

--- src/pbrt_bs.ml
+++ src/pbrt_bs.ml
@@ -21,141 +21,125 @@ module E =
         (fun exn  ->
            match exn with | Failure e -> Some (string_of_error e) | _ -> None)
   end
-let int32_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> int32= fun
-  (type a) ->
-  (fun (ty,v)  ->
+let int32_: Js.Json.tagged_t -> string -> string -> int32 =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.String  -> Int32.of_string v
-         | Js_json.Number  -> Int32.of_float v
-         | Js_json.Null  -> 0l
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONString v -> Int32.of_string v
+         | Js.Json.JSONNumber v -> Int32.of_float v
+         | Js.Json.JSONNull  -> 0l
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     int32)
 let int32 json record_name field_name =
-  int32_ (reifyType json) record_name field_name
-let int64_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> int64= fun
-  (type a) ->
-  (fun (ty,v)  ->
+  int32_ (Js.Json.classify json) record_name field_name
+let int64_: Js.Json.tagged_t -> string -> string -> int64 =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.String  -> Int64.of_string v
-         | Js_json.Number  -> Int64.of_float v
-         | Js_json.Null  -> 0L
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONString v -> Int64.of_string v
+         | Js.Json.JSONNumber v -> Int64.of_float v
+         | Js.Json.JSONNull  -> 0L
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     int64)
 let int64 json record_name field_name =
-  int64_ (Js_json.reifyType json) record_name field_name
-let float_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> float= fun
-  (type a) ->
-  (fun (ty,v)  ->
+  int64_ (Js.Json.classify json) record_name field_name
+let float_: Js.Json.tagged_t -> string -> string -> float =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.String  -> float_of_string v
-         | Js_json.Number  -> v
-         | Js_json.Null  -> 0.0
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONString v -> float_of_string v
+         | Js.Json.JSONNumber v -> v
+         | Js.Json.JSONNull  -> 0.0
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     float)
 let float json record_name field_name =
-  float_ (Js_json.reifyType json) record_name field_name
-let int_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> int= fun (type
-  a) ->
-  (fun (ty,v)  ->
+  float_ (Js.Json.classify json) record_name field_name
+let int_: Js.Json.tagged_t -> string -> string -> int =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.String  -> int_of_string v
-         | Js_json.Number  -> int_of_float v
-         | Js_json.Null  -> 0
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONString v -> int_of_string v
+         | Js.Json.JSONNumber v -> int_of_float v
+         | Js.Json.JSONNull  -> 0
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     int)
 let int json record_name field_name =
-  int_ (Js_json.reifyType json) record_name field_name
-let bool_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> bool= fun (type
-  a) ->
-  (fun (ty,v)  ->
+  int_ (Js.Json.classify json) record_name field_name
+let bool_: Js.Json.tagged_t -> string -> string -> bool =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.Boolean  -> Js.to_bool v
-         | Js_json.Null  -> false
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONTrue  -> true
+         | Js.Json.JSONFalse  -> false
+         | Js.Json.JSONNull  -> false
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     bool)
 let bool json record_name field_name =
-  bool_ (Js_json.reifyType json) record_name field_name
-let string_ : 'a . ('a Js_json.kind* 'a) -> string -> string -> string= fun
-  (type a) ->
-  (fun (ty,v)  ->
+  bool_ (Js.Json.classify json) record_name field_name
+let string_: Js.Json.tagged_t -> string -> string -> string =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.String  -> v
-         | Js_json.Null  -> ""
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONString v -> v
+         | Js.Json.JSONNull  -> ""
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     string)
 let string json record_name field_name =
-  string_ (Js_json.reifyType json) record_name field_name
+  string_ (Js.Json.classify json) record_name field_name
 let bytes _ record_name field_name =
   E.unexpected_json_type record_name field_name
-let object__ :
-  'a . ('a Js_json.kind* 'a) -> string -> string -> Js_json.t Js_dict.t= fun
-  (type a) ->
-  (fun (ty,v)  ->
+let object__: Js.Json.tagged_t -> string -> string -> Js_json.t Js_dict.t =
+  fun (type a) ->
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.Object  -> v
-         | Js_json.Null  -> Js_dict.empty ()
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONObject v -> v
+         | Js.Json.JSONNull  -> Js_dict.empty ()
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     Js_json.t
                                                                     Js_dict.t)
 let object_ json record_name field_name =
-  object__ (Js_json.reifyType json) record_name field_name
-let array__ :
-  'a . ('a Js_json.kind* 'a) -> string -> string -> Js_json.t array= fun
-  (type a) ->
-  (fun (ty,v)  ->
+  object__ (Js.Json.classify json) record_name field_name
+let array__: Js.Json.tagged_t -> string -> string -> Js_json.t array =
+  (fun tag  ->
      fun record_name  ->
        fun field_name  ->
-         match ty with
-         | Js_json.Array  -> v
-         | Js_json.Null  -> [||]
-         | _ -> E.unexpected_json_type record_name field_name : (a
-                                                                  Js_json.kind*
-                                                                  a) ->
+         match tag with
+         | Js.Json.JSONArray v -> v
+         | Js.Json.JSONNull  -> [||]
+         | _ -> E.unexpected_json_type record_name field_name : Js.Json.tagged_t
+                                                                  ->
                                                                   string ->
                                                                     string ->
                                                                     Js_json.t
                                                                     array)
 let array_ json record_name field_name =
-  array__ (Js_json.reifyType json) record_name field_name
+  array__ (Js.Json.classify json) record_name field_name
